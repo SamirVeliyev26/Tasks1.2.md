5701018629ff4430fa4c669d55d7d08d766e10ba
Samir <vliyevsamir385@gmail.com>
2023-05-07 13:22:24 +0400

    Encapsulation task

---
+++ EncapsulationTask1.2/EncapsulationTask1.2/Hotel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationTask1._2
+{
+    internal class Hotel
+    {
+
+        private int _id;
+        private int _RoomCount;
+        private string _HotelName;
+        private string _address;
+        private int _RoomPrice;
+
+
+        public int RoomCount 
+        {
+            get 
+            {
+                return _RoomCount;
+            }
+            set 
+            { 
+                if(value>=0 && value <= 200)
+                {
+                    _RoomCount = value;
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Siz isdediyiniz qeder qala bilmersiz.");
+                }
+
+
+            }
+        
+        
+        
+        }
+
+        public int RoomPrice 
+        {
+
+
+
+            get 
+            {
+                return _RoomPrice;
+            }
+            set 
+            { 
+              if (value>60 && value < 150)
+              {
+
+                    _RoomPrice = value;
+
+              }
+                else
+                {
+                    Console.WriteLine("Siz istediyiniz qiymete otaq yoxdur.");
+                }
+
+
+
+            }
+        }
+
+        public int Hotel2 
+        { 
+            get 
+            {
+                return RoomPrice * RoomCount;
+               
+            }
+
+            
+
+
+
+        }
+
+        public void Hotel3()
+        {
+
+            Console.WriteLine($"Sizin hotele borcunuz {Hotel2} azn dir");
+
+        }
+
+        
+
+
+
+
+    }
+
+}
---
+++ EncapsulationTask1.2/EncapsulationTask1.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using EncapsulationTask1._2;
+using System.Transactions;
+using static System.Console;
+using static System.Convert;
+
+Hotel hotel = new Hotel()
+{
+    RoomPrice = 62,
+    RoomCount = 87,
+    
+    
+   
+};
+
+hotel.Hotel3();
